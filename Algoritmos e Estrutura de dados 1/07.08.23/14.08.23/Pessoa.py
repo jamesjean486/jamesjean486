16fda4b4c6fb166246438c77ffb1d6490f86b146
jamesjean486 <107717316+jamesjean486@users.noreply.github.com>
2023-08-14 21:26:14 -0300

    Add files via upload

---
+++ Algoritmos e Estrutura de dados 1/07.08.23/14.08.23/Pessoa.py
@@ -0,0 +1,8 @@
+#classe sempre com letra maiuscula, e nome do arquivo  com o msm nome da classe 
+class Pessoa:
+    #definindo o método construtor
+    def __init__(self):
+        #construir os atributos tal como definir seus valores
+        #teste 
+        print("obj instanciado")
+        
